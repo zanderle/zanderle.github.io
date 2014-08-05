955371005875fe66e90e531bb750412164b35c44
zanderle <zan.anderle@gmail.com>
2014-08-05 18:51:20 +0200

    Updated disqus count

--- js/default.js
+++ js/default.js
@@ -7,4 +7,37 @@ $(document).ready(function() {
         e.preventDefault();
         $('nav.site-nav').slideToggle(200);
     });
+
+    var disqusPublicKey = "HUqw8lfxGW7tYNIJ883DE46T7sXUoxRBDGKRME2PLL8Vr2DI1uT2o68vDxYb7SbO";
+    var disqusShortname = "mymusingsonmusic"; // Replace with your own shortname
+
+    var urlArray = [];
+    $('.count-disqus-comments').each(function () {
+        var url = $(this).attr('data-disqus-url');
+        urlArray.push('link:' + url);
+    });
+
+    if (urlArray) {
+        $.ajax({
+            type: 'GET',
+            url: "https://disqus.com/api/3.0/threads/set.jsonp",
+            data: { api_key: disqusPublicKey, forum : disqusShortname, thread : urlArray },
+            cache: false,
+            dataType: 'jsonp',
+            success: function (result) {
+
+              for (var i in result.response) {
+
+                var countText = " comments";
+                var count = result.response[i].posts;
+
+                if (count == 1)
+                  countText = " comment";
+
+                $('a[data-disqus-url="' + result.response[i].link + '"]').html(count + countText);
+
+              }
+            }
+          });
+    }
 });
